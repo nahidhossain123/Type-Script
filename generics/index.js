8865b438962b99077b01c5821f167220dc2bf60f
nahidhossain123 <nahidhossain351@gmail.com>
2024-01-09 00:41:21 +0600

    DAY4

---
+++ generics/index.js
@@ -0,0 +1,6 @@
+function UserInfo2(userid, name) {
+    console.log("user Id ".concat(userid, " user name ").concat(name));
+}
+UserInfo2("101", "nahid hossain");
+UserInfo2(101, "nahid hossain");
+UserInfo2(true, 30);
---
+++ generics/index.ts
@@ -0,0 +1,7 @@
+function UserInfo2<X, Y>(userid: X, name: Y): void {
+  console.log(`user Id ${userid} user name ${name}`);
+}
+
+UserInfo2("101", "nahid hossain");
+UserInfo2(101, "nahid hossain");
+UserInfo2(true, 30);
